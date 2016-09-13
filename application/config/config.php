d1ad278599390dd483a82c01298c8b61bbcedc0f
francois <francois@LeFrank-lnx-93000>
2016-09-13 23:56:02 +0200

    Increasing session timeout to 1 day.

--- application/config/config.php
+++ application/config/config.php
@@ -245,7 +245,7 @@ $config['encryption_key'] = 'asd6f7d57fa5d7fadfa7sd8zvx8Nsdyc89sa';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 86400;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
