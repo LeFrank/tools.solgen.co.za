60419fe492a69e16c991c6aa3e6f38205a55d461
francois <francois@francois-MS-7970>
2019-12-17 23:06:37 +0200

    updating FAQ

--- application/views/faq.php
+++ application/views/faq.php
@@ -1,108 +1,236 @@
-<?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
-<div id="faq-content">
-    <h2>Frequently Asked Questions</h2>
-    <ol>
-        <li>
-            <h3>What does this site do?</h3>
-            <p>
-                The intention is to build tools that help me :) and be useful to others as well.<br/>
-                At the moment there is only one tools available. The expenses tool.<br/>
-                You input and track your expenses over a calendar month.<br/>
-                It then generates interesting statistics and graphs of this data.
-            </p>
-        </li>
-        <li>
-            <h3>What is the expense tool?</h3>
-            <p>
-                The expense tool began with a question: <strong>&lsquo;Where did the money go?&rsquo;</strong><br/>
-                Phase one of answering this question involved keeping track of expenses.<br/>
-                In the early stages, excel spread sheets were used. Which worked brilliantly for a month or two.<br/>
-                However after a few months, the data would be separated either as a document <br/>
-                per month or a sheet per month.<br/>
-            </p>
-            <p>
-                Having all expenses for multiple months on one sheet became unwieldily <br/>
-                and prone to error when capturing new data. Generating graphs and <br/>
-                analyzing over period also proved to be a manual process.
-            </p>
-            <p>
-                Once a certain level of separation sets in, it is quite difficult to get a<br/>
-                quick and concise understanding of patterns and behaviour.<br/>
-                Thus, the expense tool is an attempt to capture the data as one<br/> 
-                massive dataset, and slicing and dicing the data set based criteria <br/>
-                such as date, type of expense, payment method, location and amount.
-            </p>
-            <p>
-                With these criterion and a large dataset the ability to find patterns<br/> 
-                and draw helpful statistics becomes simple and clearly shows <br/>
-                <strong>where the money went</strong>
-            </p>
+<div class="row expanded">
+    <div class="large-12 columns" >
 
-        </li>
-        <li>
-            <h2>What does the expense tool do?</h2>
-            
-            <h3>Capture Expenses</h3>
+        <h2>Frequently Asked Questions</h2>
+        <div class="row expanded">
+            <div class="large-12 columns" >
+                <h3>What does this site do?</h3>
 
-            <p>Gives you the ability to track your expenses.<br />
-                Expenses are categorised by type, payment method and location.</p>
+                <p>The intention is to build tools that help me :) and be useful to others as well.<br />
+                    At the moment there are <strong>nine</strong> tools available.</p>
 
-            <p>Provides the ability to:</p>
+                <ol>
+                    <li>
+                        <h4>The Finances Tool.</h4>
 
-            <ul>
-                <li>Create their own expense categories.</li>
-                <li>Create additional payment methods.</li>
-                <li>Create expense periods.</li>
-            </ul>
+                        <ol>
+                            <li>
+                                <p>You input and track your expenses over a calendar month.</p>
+                            </li>
+                            <li>
+                                <p>It then generates interesting statistics and graphs of this data.</p>
+                            </li>
+                            <li>
+                                <p>Create Budgets</p>
+                            </li>
+                            <li>
+                                <p>Track expenses against your budget</p>
+                            </li>
+                            <li>
+                                <p>Review Budget overview</p>
+                            </li>
+                            <li>
+                                <p>Import expenses from Bank ( ABSA CSV )</p>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Wishlist Tool</h4>
 
-            <h3>Create and Manage Expense Periods</h3>
+                        <ol>
+                            <li>
+                                <p style="text-align: justify;">Add items to your personal wishlist.</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">This allows one to plan out expenses and avoid impulsive shopping.</p>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Notes</h4>
 
-            <p>Are date ranges for which you would like to track and group expenses.<br />
-                e.g. If you earn a weekly wages, Friday to Friday may be the best way to track and group expenses.<br />
-                Monthly, start on the day you receive your salary and end the day before the next pay date.<br />
-                You can specify what and how long the expense periods are.<br />
-                The periods are used for drawing up budgets and generating statistics.&nbsp;&nbsp; &nbsp;</p>
+                        <ol>
+                            <li>
+                                <p style="text-align: justify;">A simple note taking application</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Has a rich text editor and tags</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Searchable</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Buildable templates</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Auto-Save after first save</p>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Timetable</h4>
 
-            <h3>Budget</h3>
+                        <ol>
+                            <li>
+                                <p style="text-align: justify;">Personal Calendar</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Categories</p>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Locations</h4>
 
-            <p>You can create a budget. Setting a limit for the budget period.<br />
-                Describe some of the major planned expenses for this period. E.G. Home/Vehicle repairs, Rent/Mortgage, groceries etc.<br />
-                List a few major expected expenses.</p>
+                        <ol>
+                            <li>
+                                <p style="text-align: justify;">Save locations</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Add geolocation and location meta data</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Locations can be linked to expenses&nbsp;</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Locations can return weather data</p>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Weather</h4>
 
-            <p>Create a list of categorized budget limits.</p>
+                        <ol>
+                            <li>
+                                <p style="text-align: justify;">After adding locations you can view the forecast weather for this location</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">1 day or 7 day forecast</p>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Timeline</h4>
 
-            <ul>
-                <li>This plainly shows&nbsp;whether you accurately understand what you are spending your money on.</li>
-                <li>Helps enhance the accuracy of estimation for future budgets.</li>
-                <li>Points out the categories which are not behaving as expected. This is handy for categories like utilities, insurance. When they unexpectedly increase, it will quickly become apparent and give you an opportunity to address the sudden increase.</li>
-            </ul>
+                        <ol>
+                            <li>
+                                <p style="text-align: justify;">A zietgiest of your tool activities</p>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Resources</h4>
 
-            <p>Each budget item list has three ways of being created.<br />
-                &nbsp;&nbsp; &nbsp;1. Use the expenses from the previous period.<br />
-                &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;This is usefull as the previous budget period usually consists of +- 40% repeatable expenses and most likely limits will remain more or less the same.<br />
-                &nbsp;&nbsp; &nbsp;2. Use Previous Budget.<br />
-                &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;This will use the previously configured budget items and limits. The final budgetted item limit can still be altered.<br />
-                &nbsp;&nbsp; &nbsp;3. Use Expense Types<br />
-                &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;No limits set or brought forward. The user has no prior guidance per category.<br />
-                &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;This is also the first option which is used for a brand new budget when no prior expenses have been captured.</p>
+                        <ol>
+                            <li>
+                                <p style="text-align: justify;">A online location for resources ( ALPHA )</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Resources can be linked to certain tools</p>
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Health</h4>
 
-            <p>After the budget has been created. Go ahead and continue to capture expenses.<br />
-                The budget will reflect the current status of each category. It will show the categories budgetted limit. How much has been spent so far, how much is remaining to be spent in units and percentage.</p>
+                        <ol>
+                            <li>
+                                <p style="text-align: justify;">Metrics Tracker</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Exercise Tracker</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Diet Tracker</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Emotional Tracker</p>
+                            </li>
+                            <li>
+                                <p style="text-align: justify;">Others</p>
+                            </li>
+                        </ol>
+                    </li>
+                </ol>
 
-            <p>The remaining column is colour coded to indicate the health of each category.</p>
+                <p>&nbsp;</p>
 
-            <p>It displays the number of expenses, total spent thus far, total remaining to be spent, and what is projected if all category limits are spent. If there are overages in any one of the categories it will display how much you will be over by at the end of the period. Generally if you have gone over the budget it means, you have either gone into debt ( credit cards, dipping into your savings, loans )</p>
+                <h3>What is the expense tool?</h3>
 
-            <p>In general when you go over on one category you should attempt to reduce spending in a different category to remain within the overall budget. You should not alter the budget limits after they have been set. This is specifically done so that categories which have bust the budget remain as warnings for future budgetting and post analysis. We learn most from our mistakes. Especially if those mistakes are not hidden or retroactively corrected.</p>
+                <p>The expense tool began with a question: <strong>&lsquo;Where did the money go?&rsquo;</strong><br />
+                    Phase one of answering this question involved keeping track of expenses.<br />
+                    In the early stages, excel spread sheets were used. Which worked brilliantly for a month or two.<br />
+                    However after a few months, the data would be separated either as a document<br />
+                    per month or a sheet per month.</p>
 
-            <p>&nbsp;</p>
+                <p>Having all expenses for multiple months on one sheet became unwieldily<br />
+                    and prone to error when capturing new data. Generating graphs and<br />
+                    analyzing over period also proved to be a manual process.</p>
 
-        </li>
-    </ol>
+                <p>Once a certain level of separation sets in, it is quite difficult to get a<br />
+                    quick and concise understanding of patterns and behaviour.<br />
+                    Thus, the expense tool is an attempt to capture the data as one<br />
+                    massive dataset, and slicing and dicing the data set based criteria<br />
+                    such as date, type of expense, payment method, location and amount.</p>
+
+                <p>With these criterion and a large dataset the ability to find patterns<br />
+                    and draw helpful statistics becomes simple and clearly shows<br />
+                    <strong>where the money went</strong></p>
+
+                <h2>What does the expense tool do?</h2>
+
+                <h3>Capture Expenses</h3>
+
+                <p>Gives you the ability to track your expenses.<br />
+                    Expenses are categorised by type, payment method and location.</p>
+
+                <p>Provides the ability to:</p>
+
+                <h3>Create and Manage Expense Periods</h3>
+
+                <p>Are date ranges for which you would like to track and group expenses.<br />
+                    e.g. If you earn a weekly wages, Friday to Friday may be the best way to track and group expenses.<br />
+                    Monthly, start on the day you receive your salary and end the day before the next pay date.<br />
+                    You can specify what and how long the expense periods are.<br />
+                    The periods are used for drawing up budgets and generating statistics.&nbsp;&nbsp; &nbsp;</p>
+
+                <h3>Budget</h3>
+
+                <p>You can create a budget. Setting a limit for the budget period.<br />
+                    Describe some of the major planned expenses for this period. E.G. Home/Vehicle repairs, Rent/Mortgage, groceries etc.<br />
+                    List a few major expected expenses.</p>
+
+                <p>Create a list of categorized budget limits.</p>
+
+                <p>Each budget item list has three ways of being created.<br />
+                    &nbsp;&nbsp; &nbsp;1. Use the expenses from the previous period.<br />
+                    &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;This is usefull as the previous budget period usually consists of +- 40% repeatable expenses and most likely limits will remain more or less the same.<br />
+                    &nbsp;&nbsp; &nbsp;2. Use Previous Budget.<br />
+                    &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;This will use the previously configured budget items and limits. The final budgetted item limit can still be altered.<br />
+                    &nbsp;&nbsp; &nbsp;3. Use Expense Types<br />
+                    &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;No limits set or brought forward. The user has no prior guidance per category.<br />
+                    &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;This is also the first option which is used for a brand new budget when no prior expenses have been captured.</p>
+
+                <p>After the budget has been created. Go ahead and continue to capture expenses.<br />
+                    The budget will reflect the current status of each category. It will show the categories budgetted limit. How much has been spent so far, how much is remaining to be spent in units and percentage.</p>
+
+                <p>The remaining column is colour coded to indicate the health of each category.</p>
+
+                <p>It displays the number of expenses, total spent thus far, total remaining to be spent, and what is projected if all category limits are spent. If there are overages in any one of the categories it will display how much you will be over by at the end of the period. Generally if you have gone over the budget it means, you have either gone into debt ( credit cards, dipping into your savings, loans )</p>
+
+                <p>In general when you go over on one category you should attempt to reduce spending in a different category to remain within the overall budget. You should not alter the budget limits after they have been set. This is specifically done so that categories which have bust the budget remain as warnings for future budgetting and post analysis. We learn most from our mistakes. Especially if those mistakes are not hidden or retroactively corrected.</p>
+
+                <p>&nbsp;</p>
+
+                <ul>
+                    <li>Create their own expense categories.</li>
+                    <li>Create additional payment methods.</li>
+                    <li>Create expense periods.</li>
+                    <li>This plainly shows&nbsp;whether you accurately understand what you are spending your money on.</li>
+                    <li>Helps enhance the accuracy of estimation for future budgets.</li>
+                    <li>Points out the categories which are not behaving as expected. This is handy for categories like utilities, insurance. When they unexpectedly increase, it will quickly become apparent and give you an opportunity to address the sudden increase.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
